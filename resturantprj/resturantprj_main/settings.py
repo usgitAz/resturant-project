50e9c4d043a0ecc954c349a9fc4bb9a79b9f79a1
Az <1usasgm@gmail.com>
2023-10-02 05:59:35 +0330

    add get user current location on main page and list resturant by location

--- resturantprj/resturantprj_main/settings.py
+++ resturantprj/resturantprj_main/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG' , default=False,cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "192.168.1.3",
+    "127.0.0.1",
+]
 
 
 # Application definition
